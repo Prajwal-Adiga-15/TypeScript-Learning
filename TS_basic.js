6711eb6ceea4e195c00f798a5a0bfaaf9d56f4e1
Prajwal Adiga B P <58132921+Prajwal-Adiga-15@users.noreply.github.com>
2023-03-24 12:17:52 +0530

    formated

--- TS_basic.js
+++ TS_basic.js
@@ -2,8 +2,8 @@ var button = document.querySelector("button");
 var num1 = document.getElementById("num1");
 var num2 = document.getElementById("num2");
 function add(num1, num2) {
-    return num1 + num2;
+  return num1 + num2;
 }
 button.addEventListener("click", function () {
-    console.log(add(+num1.value, +num2.value));
+  console.log(add(+num1.value, +num2.value));
 });
--- TS_basic.ts
+++ TS_basic.ts
@@ -2,10 +2,10 @@ const button = document.querySelector("button")! as HTMLButtonElement;
 const num1 = document.getElementById("num1")! as HTMLInputElement;
 const num2 = document.getElementById("num2")! as HTMLInputElement;
 
-function add(num1: number,num2: number){
-    return num1 + num2;
+function add(num1: number, num2: number) {
+  return num1 + num2;
 }
 
-button.addEventListener("click", function(){
-    console.log(add(+num1.value, +num2.value));
+button.addEventListener("click", function () {
+  console.log(add(+num1.value, +num2.value));
 });
