87dfc1d0fe9d3e967d4c75e29e4c1e7b3641d68b
Prajwal Adiga B P <58132921+Prajwal-Adiga-15@users.noreply.github.com>
2023-03-24 22:01:05 +0530

    basics_2

---
+++ func_1.js
@@ -0,0 +1,8 @@
+function sum(n1, n2) {
+    console.log(typeof (n1));
+    return n1 + n2;
+}
+var n1 = 63;
+var n2 = 78;
+var result = sum(n1, n2);
+console.log(result);
---
+++ func_1.ts
@@ -0,0 +1,10 @@
+function sum(n1:number, n2:number){
+    console.log(typeof(n1));
+    return n1 + n2;
+}
+
+let n1 = 63;
+const n2 = 78;
+
+const result = sum(n1,n2);
+console.log(result);
