a8b3af01d48b876196efed494fce98e4e1c2765a
Prajwal Adiga B P <58132921+Prajwal-Adiga-15@users.noreply.github.com>
2023-03-24 22:25:51 +0530

    objects

---
+++ object.js
@@ -0,0 +1,6 @@
+var details = {
+    name: "Prajwal",
+    age: 22,
+    student: false
+};
+console.log(details.student, details.age);
---
+++ object.ts
@@ -0,0 +1,11 @@
+const details: {
+    name: string;
+    age: number;
+    student: boolean;
+} = {
+    name: "Prajwal",
+    age: 22,
+    student: false
+};
+
+console.log(details.student, details.age);
