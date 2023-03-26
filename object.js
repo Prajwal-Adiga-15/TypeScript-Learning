f33bafe1ea1d2d7fffabc10aae5b329d7d754ec2
Prajwal Adiga B P <58132921+Prajwal-Adiga-15@users.noreply.github.com>
2023-03-26 18:07:03 +0530

    updated objects

--- object.js
+++ object.js
@@ -1,6 +1,11 @@
 var details = {
     name: "Prajwal",
     age: 22,
-    student: false
+    student: false,
+    location: 'Bangalore',
+    work_details: {
+        work: 'Engineer',
+        salary: 300000
+    }
 };
-console.log(details.student, details.age);
+console.log(details.student, details.age, details.work_details.work);
--- object.ts
+++ object.ts
@@ -2,10 +2,20 @@ const details: {
     name: string;
     age: number;
     student: boolean;
+    location: string;
+    work_details: {
+        work: string;
+        salary: number;
+    }
 } = {
     name: "Prajwal",
     age: 22,
-    student: false
+    student: false,
+    location: 'Bangalore',
+    work_details: {
+        work: 'Engineer',
+        salary: 300000
+    }
 };
 
-console.log(details.student, details.age);
+console.log(details.student, details.age, details.work_details.work);
